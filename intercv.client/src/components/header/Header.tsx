139bb8599e2a480c6ded961ebfdf12ea99c73ee7
EricNorrwing <eric.norrwing@gmail.com>
2025-10-21 14:31:44 +0200

    minor improvements to header, added a base main page

--- intercv.client/src/components/header/Header.tsx
+++ intercv.client/src/components/header/Header.tsx
@@ -34,48 +34,15 @@ export function Header() {
                             </Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Link</a>
-                        </li>
-                        <li className="nav-item dropdown">
-                            <a
-                                className="nav-link dropdown-toggle"
-                                href="#"
-                                id="navbarDropdown"
-                                role="button"
-                                data-bs-toggle="dropdown"
-                                aria-expanded="false"
-                            >
-                                Dropdown
-                            </a>
-                            <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li><a className="dropdown-item" href="#">Action</a></li>
-                                <li><a className="dropdown-item" href="#">Another action</a></li>
-                                <li><hr className="dropdown-divider" /></li>
-                                <li><a className="dropdown-item" href="#">Something else here</a></li>
-                            </ul>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link disabled" href="#">Disabled</a>
+                            <a className="nav-link" href="#">Login</a>
                         </li>
                     </ul>
-
-                    
-                    <form className="d-flex ms-lg-3 mt-2 mt-lg-0">
-                        <input
-                            className="form-control me-2"
-                            type="search"
-                            placeholder="Search"
-                            aria-label="Search"
-                        />
-
                         <Link
                             to="/en/cv/sampleCv"
                             className="btn btn-outline-success"
                         >
                             View sample CV
                         </Link>
-                    </form>
-                    
                     <div className="ms-3 btn btn-outline-success">{lang}</div>
                 </div>
             </div>
--- intercv.client/src/pages/landing/LandingPage.tsx
+++ intercv.client/src/pages/landing/LandingPage.tsx
@@ -1,12 +1,30 @@
-﻿import {Link} from "@tanstack/react-router";
+﻿import { Link } from "@tanstack/react-router";
 
 export default function LandingPage() {
     return (
-        <div>
-            <h1> Welcome to InterCV! Click here to browse a sample CV and view the next generation CV</h1>
-            <Link className="btn" to="/en/cv/sampleCv">
+        <div className="d-flex flex-column justify-content-center align-items-center text-center vh-100 bg-light p-4">
+            
+            <h1 className="display-4 fw-bold mb-4">
+                Welcome to InterCV!
+            </h1>
+            <p className="lead mb-4">
+                Explore the next generation of interactive CVs that showcase skills, experience, and achievements dynamically.
+            </p>
+
+            
+            <Link className="btn btn-primary btn-lg mb-3" to="/en/cv/sampleCv">
                 View Sample CV
             </Link>
+
+           
+            <div className="mt-4">
+                <p className="mb-2 fw-semibold">
+                    Do you want to make your own? Join the team and create your own ICV!
+                </p>
+                <button className="btn btn-outline-success btn-lg">
+                    Join the Team
+                </button>
+            </div>
         </div>
     );
 }
