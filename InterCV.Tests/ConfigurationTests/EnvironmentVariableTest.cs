d509e1828164427c3cfce2c3c1a2348988b292ff
EricNorrwing <eric.norrwing@gmail.com>
2025-10-29 14:57:29 +0100

    cleaned up gitignore, again

--- InterCV.Tests/ConfigurationTests/EnvironmentVariableTest.cs
+++ InterCV.Tests/ConfigurationTests/EnvironmentVariableTest.cs
@@ -3,26 +3,20 @@ using Xunit.Abstractions;
 
 namespace InterCV.Tests.ConfigurationTests;
 
-public class EnvironmentVariableTest
+public class EnvironmentVariableTest(ITestOutputHelper testOutputHelper)
 {
-    private readonly ITestOutputHelper _testOutputHelper;
-
-    public EnvironmentVariableTest(ITestOutputHelper testOutputHelper)
-    {
-        _testOutputHelper = testOutputHelper;
-    }
-
+    
+    //TODO Build real tests once the configs are properly setup and the pipeline works
     [Fact]
     public void MongoDbUri_Should_BeLoaded_FromUserSecrets()
     {
         var config = new ConfigurationBuilder()
             .AddEnvironmentVariables()
-            .AddUserSecrets("32088852-2b6b-49ee-a777-5569447d312e") // <--- load secrets from server project
+            .AddUserSecrets("32088852-2b6b-49ee-a777-5569447d312e") 
             .Build();
 
         var mongoUri = config.GetConnectionString("MongoDbUri");
 
         Assert.False(string.IsNullOrEmpty(mongoUri), "MongoDbUri is not set!");
-        _testOutputHelper.WriteLine($"MongoDbUri loaded: {mongoUri.Substring(0, 10)}*****");
     }
 }
