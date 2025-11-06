1dd785d10016ac35f4f7281e9c2c885d81794888
EricNorrwing <eric.norrwing@gmail.com>
2025-11-06 12:05:32 +0100

    more work on prepping the DB, experimenting on local db to find a good setup

--- InterCV.Server/Configuration/DbContextsConfiguration/DBContext/InterCvDBContext.cs
+++ InterCV.Server/Configuration/DbContextsConfiguration/DBContext/InterCvDBContext.cs
@@ -14,17 +14,17 @@ public class InterCvDbContext(DbContextOptions<InterCvDbContext> options) : DbCo
     public DbSet<User> Users => Set<User>();
     public DbSet<AuthUser> AuthUsers => Set<AuthUser>();
     public DbSet<UserProfile> UserProfiles => Set<UserProfile>();
-        
+
     public DbSet<Cv> Cvs => Set<Cv>();
-        
+
     public DbSet<Experience> Experiences => Set<Experience>();
     public DbSet<Achievement> Achievements => Set<Achievement>();
     public DbSet<Reference> References => Set<Reference>();
-        
+
     public DbSet<Education> Educations => Set<Education>();
-        
+
     public DbSet<Tag> Tags => Set<Tag>();
-        
+
     public DbSet<CvExperience> CvExperiences => Set<CvExperience>();
     public DbSet<CvEducation> CvEducations => Set<CvEducation>();
     public DbSet<CvTag> CvTags => Set<CvTag>();
@@ -32,89 +32,37 @@ public class InterCvDbContext(DbContextOptions<InterCvDbContext> options) : DbCo
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
-            
+
         modelBuilder.Entity<User>()
             .HasOne(u => u.Auth)
             .WithOne(a => a.User)
             .HasForeignKey<AuthUser>(a => a.UserId)
-            .OnDelete(DeleteBehavior.Cascade);
+            .OnDelete(DeleteBehavior.NoAction);
 
         modelBuilder.Entity<User>()
             .HasOne(u => u.Profile)
             .WithOne(p => p.User)
             .HasForeignKey<UserProfile>(p => p.UserId)
-            .OnDelete(DeleteBehavior.Cascade);
+            .OnDelete(DeleteBehavior.NoAction);
 
         modelBuilder.Entity<User>()
             .HasMany(u => u.Cvs)
             .WithOne(c => c.User)
             .HasForeignKey(c => c.UserId)
-            .OnDelete(DeleteBehavior.Cascade);
+            .OnDelete(DeleteBehavior.NoAction);
 
         modelBuilder.Entity<User>()
             .HasMany(u => u.Experiences)
             .WithOne(e => e.User)
             .HasForeignKey(e => e.UserId)
-            .OnDelete(DeleteBehavior.Cascade);
+            .OnDelete(DeleteBehavior.NoAction);
 
         modelBuilder.Entity<User>()
             .HasMany(u => u.Educations)
             .WithOne(e => e.User)
             .HasForeignKey(e => e.UserId)
-            .OnDelete(DeleteBehavior.Cascade);
+            .OnDelete(DeleteBehavior.NoAction);
 
-        modelBuilder.Entity<CvTag>()
-            .HasKey(ct => new { ct.CvId, ct.TagId });
-
-        modelBuilder.Entity<CvTag>()
-            .HasOne(ct => ct.Cv)
-            .WithMany(c => c.Tags)
-            .HasForeignKey(ct => ct.CvId);
-
-        modelBuilder.Entity<CvTag>()
-            .HasOne(ct => ct.Tag)
-            .WithMany()
-            .HasForeignKey(ct => ct.TagId);
-            
-        modelBuilder.Entity<CvExperience>()
-            .HasKey(ce => new { ce.CvId, ce.ExperienceId });
-
-        modelBuilder.Entity<CvExperience>()
-            .HasOne(ce => ce.Cv)
-            .WithMany(c => c.Experiences)
-            .HasForeignKey(ce => ce.CvId);
-
-        modelBuilder.Entity<CvExperience>()
-            .HasOne(ce => ce.Experience)
-            .WithMany()
-            .HasForeignKey(ce => ce.ExperienceId);
-
-        modelBuilder.Entity<CvEducation>()
-            .HasKey(ce => new { ce.CvId, ce.EducationId });
-
-        modelBuilder.Entity<CvEducation>()
-            .HasOne(ce => ce.Cv)
-            .WithMany(c => c.Educations)
-            .HasForeignKey(ce => ce.CvId);
-
-        modelBuilder.Entity<CvEducation>()
-            .HasOne(ce => ce.Education)
-            .WithMany()
-            .HasForeignKey(ce => ce.EducationId);
-
-        modelBuilder.Entity<CvTag>()
-            .HasKey(ct => new { ct.CvId, ct.TagId });
-
-        modelBuilder.Entity<CvTag>()
-            .HasOne(ct => ct.Cv)
-            .WithMany(c => c.Tags)
-            .HasForeignKey(ct => ct.CvId);
-
-        modelBuilder.Entity<CvTag>()
-            .HasOne(ct => ct.Tag)
-            .WithMany()
-            .HasForeignKey(ct => ct.TagId);
-            
         modelBuilder.Entity<Experience>()
             .HasMany(e => e.Achievements)
             .WithOne(a => a.Experience)
@@ -126,10 +74,54 @@ public class InterCvDbContext(DbContextOptions<InterCvDbContext> options) : DbCo
             .WithOne(r => r.Experience)
             .HasForeignKey(r => r.ExperienceId)
             .OnDelete(DeleteBehavior.Cascade);
-            
+
+        modelBuilder.Entity<CvExperience>()
+            .HasKey(ce => new { ce.CvId, ce.ExperienceId });
+
+        modelBuilder.Entity<CvExperience>()
+            .HasOne(ce => ce.Cv)
+            .WithMany(c => c.Experiences)
+            .HasForeignKey(ce => ce.CvId)
+            .OnDelete(DeleteBehavior.NoAction);
+
+        modelBuilder.Entity<CvExperience>()
+            .HasOne(ce => ce.Experience)
+            .WithMany()
+            .HasForeignKey(ce => ce.ExperienceId)
+            .OnDelete(DeleteBehavior.NoAction);
+
+        modelBuilder.Entity<CvEducation>()
+            .HasKey(ce => new { ce.CvId, ce.EducationId });
+
+        modelBuilder.Entity<CvEducation>()
+            .HasOne(ce => ce.Cv)
+            .WithMany(c => c.Educations)
+            .HasForeignKey(ce => ce.CvId)
+            .OnDelete(DeleteBehavior.NoAction);
+
+        modelBuilder.Entity<CvEducation>()
+            .HasOne(ce => ce.Education)
+            .WithMany()
+            .HasForeignKey(ce => ce.EducationId)
+            .OnDelete(DeleteBehavior.NoAction);
+
+        modelBuilder.Entity<CvTag>()
+            .HasKey(ct => new { ct.CvId, ct.TagId });
+
+        modelBuilder.Entity<CvTag>()
+            .HasOne(ct => ct.Cv)
+            .WithMany(c => c.Tags)
+            .HasForeignKey(ct => ct.CvId)
+            .OnDelete(DeleteBehavior.NoAction);
+
+        modelBuilder.Entity<CvTag>()
+            .HasOne(ct => ct.Tag)
+            .WithMany()
+            .HasForeignKey(ct => ct.TagId)
+            .OnDelete(DeleteBehavior.NoAction);
+
         modelBuilder.Entity<Education>()
             .Property(e => e.Type)
-            .HasConversion<string>(); 
-            
+            .HasConversion<string>();
     }
 }
