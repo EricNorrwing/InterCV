e7551952ffd5f2f7f5b27d00951c9f9b9c5f540c
EricNorrwing <eric.norrwing@gmail.com>
2025-11-10 10:44:23 +0100

    adding auth0 flow to app

--- intercv.client/src/App.tsx
+++ intercv.client/src/App.tsx
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import {createRouter, RouterProvider} from "@tanstack/react-router"
 import {routeTree} from "./routeTree.gen.ts";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import { Auth0Provider} from "@auth0/auth0-react";
 
 const router = createRouter({ routeTree });
 const queryClient = new QueryClient();
@@ -10,9 +11,17 @@ const queryClient = new QueryClient();
 function App() {
     
     return (
-        <QueryClientProvider client={queryClient}>
-            <RouterProvider router={router}/>
-        </QueryClientProvider>
+        <Auth0Provider 
+            domain="intercv.eu.auth0.com" 
+            clientId="2UshsGMGoXCs90e5w0WkksP3Hji6tWG6"
+            authorizationParams={{
+                redirect_uri: window.location.origin
+            }}
+            >
+            <QueryClientProvider client={queryClient}>
+                <RouterProvider router={router}/>
+            </QueryClientProvider>
+        </Auth0Provider>
     )
 }
 
